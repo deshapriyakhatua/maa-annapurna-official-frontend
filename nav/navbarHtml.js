e2d8ab7269db533198cf8fd22f18330143de18bf
deshapriya <deshapriyakhatua@gmail.com>
2024-04-03 13:34:55 +0530

    pages added

--- nav/navbarHtml.js
+++ nav/navbarHtml.js
@@ -1,47 +1,40 @@
 document.write(`
+    
     <nav class="nav">
-    <div class="container">
-        <div class="logo">
-            <a href="#">অন্নপূর্ণা মন্দির</a>
+        <div class="container">
+            <div class="logo">
+                <a href="/maa-annapurna-official-frontend">অন্নপূর্ণা মন্দির</a>
+            </div>
+            <div id="mainListDiv" class="main_list">
+                <ul class="navlinks">
+                    <li><a href="/maa-annapurna-official-frontend/donate">দান</a></li>
+                    <li><a href="/maa-annapurna-official-frontend/festival">উৎসব</a></li>
+                    <li><a href="/maa-annapurna-official-frontend/about">বিস্তারিত</a></li>
+                    <li><a href="/maa-annapurna-official-frontend/contact">যোগাযোগ</a></li>
+                </ul>
+            </div>
+            <span class="navTrigger">
+                <i></i>
+                <i></i>
+                <i></i>
+            </span>
         </div>
-        <div id="mainListDiv" class="main_list">
-            <ul class="navlinks">
-                <li><a href="#">দান</a></li>
-                <li><a href="#">উৎসব</a></li>
-                <li><a href="#">বিস্তারিত</a></li>
-                <li><a href="#">যোগাযোগ</a></li>
-            </ul>
-        </div>
-        <span class="navTrigger">
-            <i></i>
-            <i></i>
-            <i></i>
-        </span>
-    </div>
     </nav>
 
-
+    <div class="page_language_banner">
+    <span>Change Language : <b>English</b></span>
+    </div>
     <!-- Jquery needed -->
     <script src="/maa-annapurna-official-frontend/global/js/jquery.min.js"></script>
 
-    <!-- Function used to shrink nav bar removing paddings and adding black background -->
+
     <script>
 
-    $(window).scroll(function() {
-        if ($(document).scrollTop() > 50) {
-            $('.nav').addClass('affix');
-            console.log("OK");
-        } else {
-            $('.nav').removeClass('affix');
-        }
-    });
-
-    $('.navTrigger').click(function () {
-        $(this).toggleClass('active');
-        console.log("Clicked menu");
-        $("#mainListDiv").toggleClass("show_list");
-        //$("#mainListDiv").fadeIn();
-    });
+        /* Show menu options */
+        $('.navTrigger').click(function () {
+            $(this).toggleClass('active');
+            $("#mainListDiv").toggleClass("show_list");
+        });
 
     </script>
     <script src="/maa-annapurna-official-frontend/nav/navbar.js"></script>
