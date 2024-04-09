e56ecc57f9af7e66f5de39cff916bfc0c84a9060
deshapriya <deshapriyakhatua@gmail.com>
2024-04-09 12:47:41 +0530

    changes made

--- donate/index.js
+++ donate/index.js
@@ -37,6 +37,40 @@ document.addEventListener("DOMContentLoaded", function () {
     const buttonsToObserve = Array.from(document.querySelectorAll("button , p , span, .left"));
     applyObserver(buttonObserver,buttonsToObserve);
 
+
+
+    /*----------- animation for donor container -----------*/
+
+    const colors = [
+        ["black","white"],["yellow","violet"],["red","white"],["yellow","black"],["green","white"],["orange","white"],["blue","white"],["yellow", "red"],["violet","white"],["gray","white"],["yellow","gray"],["orange","yellow"],["yellow","blue"]
+    ]
+    let animateCards = document.querySelectorAll(".donor_contianer .animate");
+
+    let cardIndex = 1;
+    let colorIndex = 0;
+
+    setInterval(()=>{
+        if(cardIndex >= animateCards.length){
+            cardIndex = 0;
+        }
+        if(colorIndex >= colors.length){
+            colorIndex = 0;
+        }
+        animateCards.forEach((elem,index)=>{
+            if(index == cardIndex){
+                elem.classList.add("start_animation");
+                elem.style.backgroundColor = colors[colorIndex][0];
+                elem.style.color = colors[colorIndex][1];
+                //elem.parentElement.style.backgroundColor = colors[colorIndex][0];
+                console.log(elem.parentElement)
+            }else{
+                elem.classList.remove("start_animation");
+            }
+        });
+        colorIndex++;
+        cardIndex++;
+    }, 4000);
+
   });
   
   
